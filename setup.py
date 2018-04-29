a937f37d97b423bb311464725d2ef6d2543ad8f8
Johan Nestaas <johannestaas@gmail.com>
2018-04-28 19:36:28 -0700

    fix project link 0.2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="dongo",
-    version="0.2.0",
+    version="0.2.1",
     description="A Django-ORM inspired Mongo ODM.",
     author="Johan Nestaas",
     author_email="johannestaas@gmail.com",
