0da67f84c60bf15c932a1159da2527568c383508
Johan Nestaas <johannestaas@gmail.com>
2018-05-12 02:36:12 -0700

    add comments to DongoBulk, and add operator stuff

--- dongo/bulk.py
+++ dongo/bulk.py
@@ -11,15 +11,16 @@ from .exceptions import DongoResultError
 __all__ = ('DongoBulk', 'DongoLazyUpdater')
 
 
-def _query_or_inst(query):
+def _inst_to_query(query):
     if '_id' in query:
         return {'_id': query['_id']}
     elif '_uuid' in query:
         return {'_uuid': query['_uuid']}
-    elif hasattr(query, '_data'):
-        return query._data
     else:
-        return query
+        raise DongoResultError(
+            'cant set in instance that was never inserted and assigned an _id '
+            'yet'
+        )
 
 
 class DongoLazyUpdater(object):
@@ -32,14 +33,21 @@ class DongoLazyUpdater(object):
         '''
         Create a lazy updater wrapper over an instance of a DongoCollection::
 
-            >> person = Person.new(name='joe')
-            >> lazy = DongoLazyUpdater(person)
-            >> lazy['foo'] = 'bar'
-            >> lazy.save()
+            person = Person.new(name='joe')
+            lazy = DongoLazyUpdater(person)
+            lazy['foo'] = 'bar'
+            lazy.save()
 
         The easier interface is to just invoke ``lazy()`` from the instance::
 
-            >> lazy = person.lazy()
+            lazy = person.lazy()
+
+        You can add lazy updaters together and perform a save all at once::
+
+            lazy1 = person1.lazy()
+            lazy2 = person2.lazy()
+            bulk = lazy1 + lazy2
+            bulk.save()
 
         :param instance: the instance to wrap
         :return: a new lazy updater
@@ -47,23 +55,29 @@ class DongoLazyUpdater(object):
         self.instance = instance
         self.ops = []
 
+    def __add__(self, other):
+        if isinstance(other, DongoBulk):
+            other.ops.extend(self.ops)
+            self.ops = []
+            return other
+        elif isinstance(other, DongoLazyUpdater):
+            bulk = DongoBulk()
+            bulk.ops.extend(self.ops + other.ops)
+            self.ops = []
+            other.ops = []
+            return bulk
+        else:
+            raise TypeError('cant add DongoLazyUpdater to anything other than '
+                            'another lazy updater or a DongoBulk instance')
+
     def __getitem__(self, attr):
         return self.instance[attr]
 
     def __setitem__(self, attr, val):
-        query = self._safe_query_or_inst()
+        query = _inst_to_query(self.instance)
         upd = UpdateOne(query, {'$set': {attr: val}}, upsert=False)
         self.instance._set_nested(attr, val)
         self.ops.append(upd)
-
-    def _safe_query_or_inst(self):
-        query = _query_or_inst(self.instance)
-        if not ('_id' in query or '_uuid' in query):
-            raise DongoResultError(
-                'cant set in instance that was never inserted and assigned '
-                'an _id yet'
-            )
-        return query
 
     def set(self, **kwargs):
         '''
@@ -72,10 +86,10 @@ class DongoLazyUpdater(object):
         :param **kwargs: the fields to update with their value
         :return: the result of ``pymongo.UpdateOne``
         '''
-        query = self._safe_query_or_inst()
+        query = _inst_to_query(self.instance)
         dct = self.instance._translate_dunder_dict(kwargs)
         for key, val in dct.items():
-            self._set_nested(key, val)
+            self.instance._set_nested(key, val)
         upd = UpdateOne(query, {'$set': kwargs}, upsert=False)
         self.ops.append(upd)
         return upd
@@ -99,43 +113,169 @@ class DongoBulk(object):
     '''
 
     def __init__(self, klass, ops=None):
-
         self.klass = klass
         self.ops = ops or []
 
-    def update_one(self, query, _upsert=False, **kwargs):
-        query = _query_or_inst(query)
-        upd = UpdateOne(query, {'$set': kwargs}, upsert=_upsert)
+    def __add__(self, other):
+        if isinstance(other, (DongoLazyUpdater, DongoBulk)):
+            self.ops.extend(other.ops)
+            other.ops = []
+            return self
+        else:
+            raise TypeError('cant add DongoBulk to anything other than a '
+                            'lazy updater or another DongoBulk instance')
+
+    def take(self, lazy):
+        '''
+        Take the bulk operations that a DongoLazyUpdater or other DongoBulk was
+        going to perform to aggregate into one DongoBulk::
+
+            person1 = Person.new(name='joe')
+            person2 = Person.new(name='jill')
+            lazy1 = person1.lazy()
+            lazy2 = person2.lazy()
+            lazy1.set(name='joejoe', age=30)
+            lazy2.set(age=50)
+            lazy1['foo'] = 'bar'
+
+            bulk = Person.bulk()
+            bulk.take(lazy1)
+            bulk.take(lazy2)
+
+            bulk2 = Person.bulk()
+            bulk2.update_one(person1, name='hello')
+            bulk.take(bulk2)
+
+            bulk.save()
+
+            lazy1['favorite_number'] = 12
+            # this only performs the one ``favorite_number`` update
+            lazy1.save()
+
+        The shorthand for take is simply addition assignment::
+
+            bulk += lazy1 + bulk2 + lazy2 + bulk3
+            bulk.save()
+
+        You can simply add DongoLazyUpdaters together as well::
+
+            (lazy1 + lazy2).save()
+
+        :param lazy: the ``DongoLazyUpdater`` or other ``DongoBulk``
+        '''
+        self.ops.extend(lazy.ops)
+        lazy.ops = []
+
+    def update_one(self, instance, **kwargs):
+        '''
+        Update a single instance with a deferred update operation::
+
+            person1 = Person.new(name='joe', age=30)
+            person2 = Person.new(name='jill', age=40)
+            bulk = Person.bulk()
+            bulk.update_one(person, name='joejoe')
+            bulk.update_one(person2, name='jilljill', age=41)
+            bulk.save()
+            Person.refresh_all_from_db([person1, person2])
+            # joe is named joejoe, and jill is named jilljill and 41 years old
+
+        :param instance: the instance to add an update op to
+        :return: the ``pymongo.UpdateOne`` result
+        '''
+        query = _inst_to_query(instance)
+        upd = UpdateOne(query, {'$set': kwargs}, upsert=False)
         self.ops.append(upd)
         return upd
 
-    def inc_one(self, query, _upsert=False, **kwargs):
-        query = _query_or_inst(query)
-        upd = UpdateOne(query, {'$inc': kwargs}, upsert=_upsert)
+    def inc_one(self, instance, **kwargs):
+        '''
+        Increment a single instance with an amount::
+
+            person1 = Person.new(name='joe', age=30)
+            person2 = Person.new(name='jill', age=40)
+            bulk = Person.bulk()
+            bulk.inc_one(person, age=1)
+            bulk.inc_one(person2, age=1)
+            bulk.save()
+            Person.refresh_all_from_db([person1, person2])
+            # Now joe and jill are 1 year older
+
+        :param instance: the instance to add an increment op to
+        :return: the ``pymongo.UpdateOne`` result
+        '''
+        query = _inst_to_query(instance)
+        upd = UpdateOne(query, {'$inc': kwargs}, upsert=False)
         self.ops.append(upd)
         return upd
 
-    def replace_one(self, query, _upsert=False, **kwargs):
-        query = _query_or_inst(query)
-        rep = ReplaceOne(query, kwargs, upsert=_upsert)
+    def replace_one(self, instance, **kwargs):
+        '''
+        Replace a single instance's document entirely::
+
+            person1 = Person.new(name='joe', age=30)
+            person2 = Person.new(name='jill', age=40)
+            bulk = Person.bulk()
+            bulk.replace_one(person, name='joejoe', age=50)
+            bulk.replace_one(person2, name='jilly', age=60)
+            bulk.save()
+            Person.refresh_all_from_db([person1, person2])
+            # Now their documents are overwritten entirely
+
+        :param instance: the instance to add a replace op to
+        :return: the ``pymongo.ReplaceOne`` result
+        '''
+        query = _inst_to_query(instance)
+        rep = ReplaceOne(query, kwargs, upsert=False)
         self.ops.append(rep)
         return rep
 
-    def delete_one(self, *args, **kwargs):
-        if args:
-            query = _query_or_inst(query)
-        else:
-            query = kwargs
+    def delete_one(self, instance):
+        '''
+        Delete a document.
+
+            person1 = Person.new(name='joe', age=30)
+            person2 = Person.new(name='jill', age=40)
+            bulk = Person.bulk()
+            bulk.update_one(person, name='joejoe', age=50)
+            bulk.delete_one(person2)
+            bulk.save()
+            Person.refresh_all_from_db([person1])
+            # Now jill is gone
+
+        :param instance: the instance to add a delete op to
+        :return: the ``pymongo.DeleteOne`` result
+        '''
+        query = _inst_to_query(instance)
         delete = DeleteOne(query)
         self.ops.append(delete)
         return delete
 
     def delete_many(self, **kwargs):
-        delete = DeleteMany(kwargs)
+        '''
+        Delete many documents.
+
+            person1 = Person.new(name='joe', age=30)
+            person2 = Person.new(name='jill', age=31)
+            person3 = Person.new(name='bob', age=50)
+            bulk = Person.bulk()
+            bulk.delete_many(age__gt=30)
+            bulk.save()
+            # Now jill and bob are gone, having age > 30
+
+        :param **kwargs: the query to run the delete many with
+        :return: the ``pymongo.DeleteMany`` result
+        '''
+        query, _ = self.klass._build_query(kwargs)
+        delete = DeleteMany(query)
         self.ops.append(delete)
         return delete
 
     def save(self):
+        '''
+        Perform all the saved bulk operations.
+
+        :return: the result from the ``pymongo.bulk_write``
+        '''
         result = None
         if self.ops:
             result = self.klass.coll.bulk_write(self.ops)
